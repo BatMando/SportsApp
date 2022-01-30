6e3bcca3ab6f36a9447fa837420d398145ba2c7c
admin <admin@admins-Mac.local>
2022-01-30 04:09:32 -0800

    manual merge storyboard

---
+++ SportsApp/View/Xibs/TeamCollectionViewCell/TeamCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TeamCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by admin on 1/29/22.
+//  Copyright © 2022 admin. All rights reserved.
+//
+
+import UIKit
+
+class TeamCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+
+}
